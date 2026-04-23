f8e53d1f598022caf159c95b6230fb8b946d2aa3
JyotsnaKumbhar <72298438+JyotsnaKumbhar@users.noreply.github.com>
2026-04-23 15:26:28 +0530

    Add files via upload
    Main and app

---
+++ React-Query/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductList from "./pages/ProductList";
+import ProductDetail from "./pages/ProductDetail";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* 📋 List Page */}
+        <Route path="/" element={<ProductList />} />
+
+        {/* 🔍 Detail Page */}
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
